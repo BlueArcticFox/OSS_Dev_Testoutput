ed3afc6e6216320b54df3faedfb7b2dcf6c829f1
BlueArcticFox <100742374+BlueArcticFox@users.noreply.github.com>
2022-05-04 10:47:18 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name='OSS_Dev_Testoutput',
+  version='0.0.1',
+  description='oss-dev my OSS_Dev_Testoutput',
+  author='BlueArcticFox',
+  author_email='jominhyeok0103@gmail.com',
+  url="https://github.com/BlueArcticFox/OSS_Dev_Testoutput",
+  download_url = "https://github.com/BlueArcticFox/OSS_Dev_Testoutput/archive/refs/tags/v0.0.1.zip",
+  install_requires=['pytest'],
+  long_description='oss-dev Testoutput python module',
+  long_description_content_type = 'text/markdonw',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
